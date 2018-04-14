7ba02fd7f96316fa8665b364f980564307848dd3
Dmitry Savitski <dimasavitski@gmail.com>
2018-04-14 11:04:19 +0300

    Added .solhint and connection config

--- truffle.js
+++ truffle.js
@@ -1,4 +1,12 @@
 module.exports = {
+    networks: {
+        development: {
+            host: 'localhost',
+            port: 9545,
+            gas: 660000,
+            network_id: '*'
+        }
+    }
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
 };
